8342d228220008d88e70db0d838d42bd8537efe2
norahackett28 <norahackett84@gmail.com>
2022-11-20 14:13:47 +0000

    add about page

---
+++ app/src/main/java/org/wit/hikingtrail/views/about/AboutViewModel.kt
@@ -0,0 +1,15 @@
+package org.wit.hikingtrail.views.about
+
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AboutViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
